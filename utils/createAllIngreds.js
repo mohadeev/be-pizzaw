b414ab644b21495129fdec6bc64c2fb55818d91c
mohadeev <94206815+mohadeev@users.noreply.github.com>
2023-05-04 17:30:01 +0200

    DDF

--- utils/createAllIngreds.js
+++ utils/createAllIngreds.js
@@ -1,16 +1,22 @@
+import path, { dirname } from "path";
 import ingredModal from "../db/schema/ingredModal.js";
 import fs from "fs";
+import { fileURLToPath } from "url";
 
-// Read the JSON file from disk
-const jsonString = fs.readFileSync("/statics/ingreds.json", "utf-8");
+// Read the JSON file from disk fs.readFileSync("./statics/ingreds.json", "utf-8");
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const jsonString = fs.readFileSync(
+  path.join(__dirname, "ingreds.json"),
+  "utf-8"
+);
 const createAllIngreds = async () => {
   const data = JSON.parse(jsonString);
   ingredModal.find({}).then(async (ingreds) => {
     if (ingreds && ingreds.length > 0) {
       const tergetedChild = ingreds[0];
       const documentId = tergetedChild._id;
-
       try {
         const fillter = { _id: documentId };
         const update = tergetedChild;
